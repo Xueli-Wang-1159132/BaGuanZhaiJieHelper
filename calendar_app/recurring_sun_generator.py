e3061dc33e00a27e88f9f61bb1edd8af1c32be22
Arthur Wang <Arthur.Wang@fphcare.co.nz>
2025-04-13 07:41:48 +1200

    adding note for latitude

--- calendar_app/recurring_sun_generator.py
+++ calendar_app/recurring_sun_generator.py
@@ -92,7 +92,7 @@ class RecurringSunEventGenerator:
             f"日落: {sun_times['sunset']['str']}\n"
             f"明日日出: {sun_times['next_sunrise']['str']}\n"
             f"总时长: {sun_times['sunrise_diff']['str']}\n"
-            f"注：由于不同经纬度以及海拔会导致时间有略微差异\n"
+            f"注：由于不同经纬度以及海拔会导致时间有略微差异, 但几乎都在+-1分钟之内\n"
             f"本次时间使用的是Auckland Britomart Train Station（36°84'40.4\"S 174°76'74.0\"E）的时间\n"
             f"后续版本会增加地理位置信息以及海拔高度来提供更加准确的时间\n"
         )
